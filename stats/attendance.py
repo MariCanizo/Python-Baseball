b10b73667dc231071ab17de3c2b4996b131fbf5f
Marina Cañizo <47178280+MariCanizo@users.noreply.github.com>
2020-02-08 10:20:37 +0100

    Update attendance.py

---
+++ stats/attendance.py
@@ -0,0 +1,19 @@
+# How has All-star game attendance changed over time?
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from data import games
+
+attendance = games.loc[games['type'] == 'info' & games['multi2'] == 'attendance', ['year', 'multi3']]
+
+# Change colmuns names
+attendance.columns = ['year', 'attendance']
+
+# Convert to Numeric
+attendance.loc[:, 'attendance'] = pd.to_numeric(attendance.loc[:, 'attendance'])
+
+attendance.plot(x='year', y='attendance', figsize=(15, 7), kind='bar')
+plt.xlabel('Year')
+plt.ylabel('attendance')
+plt.axhline(y=attendance['attendance'].mean(), label='Mean', linestyle='--', color='green')
+plt.show()
