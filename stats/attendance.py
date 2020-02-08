8c9a6f8c596dff903b2405f0ca4e8aa19fdde53a
Marina Cañizo <47178280+MariCanizo@users.noreply.github.com>
2020-02-08 13:50:19 +0100

    Attendance - Select and Plot Data
    Changed plt.ylabel('attendance') -> 'Attendance'

--- stats/attendance.py
+++ stats/attendance.py
@@ -4,9 +4,6 @@ import matplotlib.pyplot as plt
 
 import data
 
-#print(data.games['type'])
-
-#x = data.games['type'] == 'info' and data.games['multi2'] == 'attendance'
 attendance = data.games.loc[(data.games['type'] == 'info') & (data.games['multi2'] == 'attendance'), ['year', 'multi3']]
 
 # Change colmuns names
@@ -16,7 +13,8 @@ attendance.columns = ['year', 'attendance']
 attendance.loc[:, 'attendance'] = pd.to_numeric(attendance.loc[:, 'attendance'])
 
 attendance.plot(x='year', y='attendance', figsize=(15, 7), kind='bar')
+
 plt.xlabel('Year')
-plt.ylabel('attendance')
+plt.ylabel('Attendance')
 plt.axhline(y=attendance['attendance'].mean(), label='Mean', linestyle='--', color='green')
 plt.show()
