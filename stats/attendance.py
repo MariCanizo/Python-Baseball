6c8b6d60d55ced1cb16b8f57f1300fcccc2d9c39
Marina Cañizo <47178280+MariCanizo@users.noreply.github.com>
2020-02-08 12:36:48 +0100

    Attendance - Select and Plot Data

--- stats/attendance.py
+++ stats/attendance.py
@@ -2,9 +2,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from data import games
+import data
 
-attendance = games.loc[games['type'] == 'info' & games['multi2'] == 'attendance', ['year', 'multi3']]
+#print(data.games['type'])
+
+#x = data.games['type'] == 'info' and data.games['multi2'] == 'attendance'
+attendance = data.games.loc[(data.games['type'] == 'info') & (data.games['multi2'] == 'attendance'), ['year', 'multi3']]
 
 # Change colmuns names
 attendance.columns = ['year', 'attendance']
